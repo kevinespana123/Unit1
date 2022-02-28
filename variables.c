43da6359c09d42adb36d0128c335d6158cfc474b
KevinEsp21 <98334902+KevinEsp21@users.noreply.github.com>
2022-02-28 07:34:05 -0600

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //its just comment.
+  //Datatype
+  //enteros, decimales, caracters, stringd
+  //Datatype varName; Datatype varName - anyValue
+  //definition and declaration
+
+  //Declaracion
+  //Definicion
+  int temp = 35; // %i, %d
+  float pi = 3.1416; // %f
+  char initial = 'M'; // %c
+  char* name= "Mariana Cuevas"; // %s
+  //otros formatos de textos//
+  // \n: salto de linea
+
+
+  int main(){
+      //ejecucion/Llamado:
+      printf("Temperatura: %i\n", temp);
+      temp = 15;
+
+      return 0;
+  }
