b37559fb5b6648a807f0fe4c627dc3b8be3fbd25
kevinespana123 <78041448+kevinespana123@users.noreply.github.com>
2022-04-06 18:57:00 -0500

    Add files via upload

---
+++ Quiz2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(){
+
+    user markovito;
+    markovito.name = "markovito";
+    markovito.type = "ServiceRobot"
+    markovito.height = 1,2;
+    markovito.weight = 2,5;
+    markovito.degreesOFFreedom = 3;
+
+    markovito . sayHelloToRobot = sayHelloToRobot;
+
+    user tiago;
+    tiago.Name = "tiago";
+    tiago.type = "ServiceRobot";
+    tiago.height = 1,7;
+    tiago.weight = 2,8;
+    tiago.degreesOFFreedom = sayHelloRobot;
+
+    markovito.sayHelloToRobot(tiago);
+
+    return 0;
+
+
+}
